304bd1f977de144cfbd12bcb8202a90176bcb3d7
moepi <martin.dahse@gmail.com>
2012-07-04 15:05:42 +0200

    more pads supported now. code ugly though

--- buildmeapdf.sh
+++ buildmeapdf.sh
@@ -4,13 +4,36 @@ PDFLATEXBIN="`which pdflatex`"
 PDFVIEWER="`which zathura`"
 TMPDIR="/tmp/buildmeapdf"
 TMPFILENAME="pad"
-URL=$1
-#piratenpad URL layout
-#https://piratenpad.de/$ID
-#https://piratenpad.de/ep/pad/export/$ID/latest?format=txt
+
+function usage {
+cat << EOF
+usage: $0 url
+This is a stupid little bash script getting tex-source from an etherpads/etherpad-lites and building a pdf.
+EOF
+exit 1
+}
+
+[[ $# -eq 1 ]] && URL=$1 || usage
+
+# for now we just sed over the URL multiple times
+# the one that matches wins
+# etherpad URL layout
+# https://piratenpad.de/ID
+# to
+# https://piratenpad.de/ep/pad/export/ID/latest?format=txt
 #
+# this one is ugly - has anyone a better idea?
+
 PADEXP="`echo $URL | sed 's/.*piratenpad.de\/\(.*\).*/https:\/\/piratenpad.de\/ep\/pad\/export\/\1\/latest?format=txt/'`"
+PADEXP="`echo $PADEXP | sed 's/.*piratepad.net\/\(.*\).*/https:\/\/piratepad.net\/ep\/pad\/export\/\1\/latest?format=txt/'`"
+PADEXP="`echo $PADEXP | sed 's/.*titanpad.com\/\(.*\).*/https:\/\/titanpad.com\/ep\/pad\/export\/\1\/latest?format=txt/'`"
+
+# etherpad lite URL layout
+# http://beta.etherpad.org/p/ID
+# to
+# http://beta.etherpad.org/p/ID/export/txt
 #
+PADEXP="`echo $PADEXP | sed 's/\(http.*\/r\/[a-Z0-9]*$\)/\1\/export\/txt/'`"
 
 mkdir -p $TMPDIR
 TMPFOLDER="`mktemp -d --tmpdir=$TMPDIR`"
@@ -19,6 +42,6 @@ BUILDPDF="$PDFLATEXBIN -interaction=batchmode -output-directory $TMPFOLDER $TMPF
 
 echo "tempfolder: $TMPFOLDER"
 
-$WGETTEX || exit 1
-$BUILDPDF || exit 2
+$WGETTEX || exit 2
+$BUILDPDF || exit 3
 rm $TMPFOLDER/*.{aux,log} && $PDFVIEWER $TMPFOLDER/$TMPFILENAME.pdf
